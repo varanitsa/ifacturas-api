2a8991076ee7f1dc95a5cf5fad4ec7fc4b096173
varanitsa <varanitsa.es@gmail.com>
2024-09-20 15:55:06 +0200

    Added HTTP and HTTPS profiles and Swagger server list to switch between HTTP and HTTPS.
    Created profiles for HTTP on port 8080 and HTTPS on port 8443. Configured Tomcat to support both protocols.

--- src/main/java/com/arquitecturajava/rest/RestApplication.java
+++ src/main/java/com/arquitecturajava/rest/RestApplication.java
@@ -1,14 +1,25 @@
 package com.arquitecturajava.rest;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 
 
-
-@OpenAPIDefinition
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "http://localhost:8080", description = "HTTP Server"),
+				@Server(url = "https://localhost:8443", description = "HTTPS Server")
+		}
+)
 @SpringBootApplication
+
+
 
 
 public class RestApplication {
@@ -17,4 +28,22 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 	}
 
+
+	@Bean
+	@Profile("http")
+	public TomcatServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		factory.addAdditionalTomcatConnectors(httpConnector());
+		return factory;
+	}
+
+	private Connector httpConnector() {
+		Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+		connector.setScheme("http");
+		connector.setPort(8080);
+		connector.setSecure(false);
+		connector.setRedirectPort(8443);
+		return connector;
+	}
 }
+
